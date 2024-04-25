aad2dfd6c5c6074b8a85490c73106eeca284d0dc
tranthienminh135 <tranthienminh135@gmail.com>
2024-04-25 11:41:40 +0700

    update

--- src/main/java/com/phuong/livestreamapi/repository/IAppUserRepository.java
+++ src/main/java/com/phuong/livestreamapi/repository/IAppUserRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface IAppUserRepository extends JpaRepository<AppUser, Integer> {
 
     @Query(value = " select au.* from app_user au where au.username = :username ", nativeQuery = true)
     AppUser getAppUserByUsername(@Param("username") String username);
 
+    @Query(value = "select au from AppUser au where au.username != :name")
+    List<AppUser> getAllUser(@Param("name") String name);
 }
